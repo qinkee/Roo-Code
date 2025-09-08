2b22abb86c87e4e4853242b4653a8ac8fce6d4cf
13625886@qq.com <egooler@hotmail.com>
2025-09-08 08:45:54 +0800

    修改.gitignore保留构建文件夹,用于CI发版

---
+++ packages/build/dist/index.d.ts
@@ -0,0 +1,3 @@
+export { getGitSha } from "./git.js";
+export { copyPaths, copyWasms, copyLocales, setupLocaleWatcher, generatePackageJson } from "./esbuild.js";
+//# sourceMappingURL=index.d.ts.map
